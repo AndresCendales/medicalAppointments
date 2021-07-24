b589ca52253e7d3b14235515385f1f0c58a872e1
Andrés Cendales <andres01660@gmail.com>
2021-07-23 21:45:19 -0500

    add interface

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,10 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(String name){
+
+    }
+
     public void showName(){
         System.out.println(name);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import ui.UIMenu;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Andrés";
-        myDoctor.showName();
+        UIMenu.showMenu();
     }
 }
